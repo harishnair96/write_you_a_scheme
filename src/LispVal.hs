b4e360c1ef9d47e5f422e233f87e9c3388f1e0fc
Harish Nair <harishnair96@gmail.com>
2021-11-19 08:57:21 +0530

    Implement a show instance for LispVal

--- src/LispVal.hs
+++ src/LispVal.hs
@@ -1,6 +1,7 @@
 module LispVal where
 
-import Control.Monad.Reader
+import Control.Monad.Reader ( ReaderT )
+import Data.List (intercalate, intersperse)
 import qualified Data.Map.Strict as Map
 import qualified Data.Text as T
 
@@ -12,10 +13,21 @@ data LispVal
   | Fun IFunc
   | Lambda IFunc EnvCtx
   | Nil
-  | Bool Bool 
+  | Bool Bool
 
 newtype IFunc = IFunc {fn :: [LispVal] -> Eval LispVal}
 
 newtype Eval a = Eval {unEval :: ReaderT EnvCtx IO a}
 
 type EnvCtx = Map.Map T.Text LispVal
+
+instance Show LispVal where
+  show val = case val of
+    Atom atom -> show atom
+    String str -> "\\" ++ show str ++ "\\" -- TODO: Avoid using ++
+    Number num -> show num
+    Bool bool -> if bool then "#t" else "#f"
+    Nil -> "Nil"
+    List lst -> "(" ++ intercalate "," (fmap show lst) ++ ")" -- TODO: Avoid using ++
+    Fun _ -> "(internal function)"
+    Lambda _ _ -> "(lambda function)"
