a7d813af2bdc95797b8270a5ecc4df78f4519870
Harish Nair <harishnair96@gmail.com>
2021-11-21 12:06:51 +0530

    Implement eval for quote and write

--- src/Eval.hs
+++ src/Eval.hs
@@ -1,4 +1,7 @@
-import LispVal (Eval, LispVal (Bool, List, Nil, Number, String))
+{-# LANGUAGE OverloadedStrings #-}
+
+import LispVal (Eval, LispVal (Atom, Bool, List, Nil, Number, String))
+import qualified Data.Text as T
 
 eval :: LispVal -> Eval LispVal
 eval lispVal = case lispVal of
@@ -7,3 +10,7 @@ eval lispVal = case lispVal of
   bool@(Bool _) -> return bool
   Nil -> return Nil
   List [] -> return Nil -- Empty lists are treated as Nil
+  List [Atom "quote", val] -> return val -- Return quoted value as such
+  List [Atom "write", val] -> return (String $ T.pack $ show val)
+  List (Atom "write":rest) -> return (String $ T.pack $ show $ List rest)
+  _ -> _
